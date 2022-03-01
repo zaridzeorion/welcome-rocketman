63e9a25f297863faa6a5823db4a4bd6057146239
Zurab Zaridze <zaridzezurab0@gmail.com>
2022-03-01 23:27:18 +0400

    SubbmittedApplications.js: retrieving submitted applications data

--- src/components/SubmittedApplications.js
+++ src/components/SubmittedApplications.js
@@ -1,13 +1,23 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
 
 const SubbmittedApplications = () => {
+  const API_TOKEN = "89nOpas%7Casdanjjh%5E%26as";
+
+  const APPLICATIONS_REQUEST_URL = `https://bootcamp-2022.devtest.ge/api/applications?token=${API_TOKEN}`;
+  const { data: users, error, loading } = useFetch(APPLICATIONS_REQUEST_URL);
+
+  const TECHNOLOGIES_REQUEST_URL = `https://bootcamp-2022.devtest.ge/api/skills`
+  const { technologies } = useFetch(TECHNOLOGIES_REQUEST_URL)
+
   return (
     <div>
       <h1>Submitted Applications</h1>
 
       <ul>
-
+        
       </ul>
+                
     </div>
   );
 };
