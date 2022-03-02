01bbb300536edb409187b14e707c1b24153042a8
Zurab Zaridze <zaridzezurab0@gmail.com>
2022-03-02 17:55:57 +0400

    Added Covid page...
    ...with only basic code. No validation handling, no navigation. Only text

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ import MainPage from "./components/MainPage";
 import SubbmittedApplications from "./components/SubmittedApplications";
 import PersonalInformation from "./components/PersonalInformation";
 import TechnicalSkillset from "./components/TechnicalSkillset";
+import Covid from "./components/Covid";
 
 function App() {
   const dispatch = useDispatch();
 
   return (
     <div className="App">
-      <TechnicalSkillset />
+      <Covid />
     </div>
   );
 }
---
+++ src/components/Covid.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Covid = () => {
+  return (
+    <>
+      <div>
+        <h2>Covid Stuff</h2>
+
+        <h5>How would you prefer to work?</h5> <br />
+        <input type="checkbox" />
+        <label>From Sairme Office</label> <br />
+        <input type="checkbox" />
+        <label>From Home</label> <br />
+        <input type="checkbox" />
+        <label>Hybrid</label>
+
+        <h5>Did you contanct covid 19? :(</h5> <br />
+        <input type="checkbox" /> 
+        <label>Yes</label> <br />
+        <input type="checkbox" />  
+        <label>No</label>
+
+        <h5>When?</h5> <br/>
+        <input type="date" />
+
+        <h5>Have you been vaccinated?</h5> <br />
+        <input type="checkbox" /> 
+        <label>Yes</label> <br />
+        <input type="checkbox" />  
+        <label>No</label>
+
+        <h5>When did you get your last covid vaccine?</h5> <br/>
+        <input type="date" />
+      </div>
+
+      <div>
+        <h2>Redberry Covid Policies</h2>
+        <p>
+          As this infamous pandemic took over the world, we adjusted our working
+          practices so that our employees can be as safe and comfortable as
+          possible. We have a hybrid work environment, so you can either work
+          from home or visit our lovely office on Sairme Street. If it was up to
+          us, we would love you to see us in the office because we think
+          face-to-face communications {`>`} Zoom meetings. Both on the fun and
+          productivity scales.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Covid;
