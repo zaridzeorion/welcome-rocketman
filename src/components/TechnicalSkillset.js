5faa7e6bb160184942707111fd5979678c23c81c
Zurab Zaridze <zaridzezurab0@gmail.com>
2022-03-05 15:55:45 +0400

    Added basic validation logic in TechnicalSkillset page

--- src/components/TechnicalSkillset.js
+++ src/components/TechnicalSkillset.js
@@ -1,33 +1,106 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import applicantSlice, { setSkills } from "../store/slices/applicantSlice";
 
 const TechnicalSkillset = () => {
+  const dispatch = useDispatch();
+  const applicant = useSelector((state) => state.applicant);
+
+  const SKILLS_REQUEST_URL = `https://bootcamp-2022.devtest.ge/api/skills`;
+  const { data: technologies } = useFetch(SKILLS_REQUEST_URL);
+
+  const [id, setId] = useState(0);
+
+  const [technology, setTechnology] = useState("");
+  const [experience, setExperience] = useState("");
+
+  const addSkill = () => {
+    const onlyNumsRe = /^[1-9]+[0-9]*$/;
+
+    if (onlyNumsRe.test(experience) && technology) {
+      dispatch(
+        setSkills([
+          ...applicant.skills,
+          { title: technology, experience: experience, id: id },
+        ])
+      );
+      setId(id + 1);
+    }
+
+    // If skill is already addded:
+    applicant.skills.find((skill) => {
+      if (skill && skill.title.toLowerCase() === technology.toLowerCase()) {
+        dispatch(setSkills([...applicant.skills]));
+      }
+    });
+  };
+
+  const delSkill = (id) => {
+    const filteredUserSkills = applicant.skills.filter((skill) => {
+      return skill.id !== id;
+    });
+
+    dispatch(setSkills(filteredUserSkills));
+  };
+
   return (
     <>
       <div>
         <h2>Tell us about your skills</h2>
-        <select>
-          <option>Skills</option>
-          <option>React</option>
-          <option>PHP</option>
+        <select
+          defaultValue={technology}
+          onChange={(e) => setTechnology(e.target.value)}
+        >
+          <option value="" disabled>
+            Skills
+          </option>
+          {technologies &&
+            technologies.map((option) => (
+              <option key={option.id} value={option.title}>
+                {option.title}
+              </option>
+            ))}
         </select>
         <br />
-        <input type="text" placeholder="Experience Duration in Years" /> <br />
-        <button>Add Programming Language</button>
-        <ul></ul>
-
-
-
-
+        <input
+          value={experience}
+          onChange={(e) => setExperience(e.target.value)}
+          type="text"
+          placeholder="Experience Duration in Years"
+        />{" "}
+        <br />
+        <button onClick={() => addSkill()}>Add Programming Language</button>
         <ul>
-          <li><Link to="/personal-information">Prev</Link></li>
-          <li><Link to="/personal-information">personal-information</Link></li>
-          <li><Link to="/skillset">skillset</Link></li>
-          <li><Link to="/covid">covid</Link></li>
-          <li><Link to="/insights">insights</Link></li>
+          {applicant.skills &&
+            applicant.skills.map((skill) => (
+              <li key={skill.id}>
+                {skill.title} {skill.experience}{" "}
+                <button onClick={() => delSkill(skill.id)}>Del</button>
+              </li>
+            ))}
+        </ul>
+        <ul>
+          <li>
+            <Link to="/personal-information">Prev</Link>
+          </li>
+          <li>
+            <Link to="/personal-information">personal-information</Link>
+          </li>
+          <li>
+            <Link to="/skillset">skillset</Link>
+          </li>
+          <li>
+            <Link to="/covid">covid</Link>
+          </li>
+          <li>
+            <Link to="/insights">insights</Link>
+          </li>
           <li></li>
-          <li><Link to="/covid">Next</Link></li>
+          <li>
+            <Link to="/covid">Next</Link>
+          </li>
         </ul>
       </div>
 
@@ -40,11 +113,6 @@ const TechnicalSkillset = () => {
         technologies like Docker and Kubernetes, and now we have set foot in the
         web3 industry.
       </div>
-
-
-
-
-
     </>
   );
 };
