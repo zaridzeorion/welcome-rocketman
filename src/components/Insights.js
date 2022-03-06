a9191d11d193aef9d1e983b6264380f275490f5a
Zurab Zaridze <zaridzezurab0@gmail.com>
2022-03-06 15:08:09 +0400

    Added validation logic in Insights page

--- src/components/Insights.js
+++ src/components/Insights.js
@@ -1,28 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { setWillOrganizeDevtalk, setDevtalkTopic, setSomethingSpecial } from "../store/slices/applicantSlice";
 
 const PersonalInformation = () => {
+  const dispatch = useDispatch();
+  const applicant = useSelector((state) => state.applicant);
+  const { will_organize_devtalk, devtalk_topic, something_special } = applicant;
+
+  const handleWillOrganizeDevtalk = e => {
+    let checked = e.target.value
+
+    checked === 'yes' && dispatch(setWillOrganizeDevtalk(true)) 
+    checked === 'no' && dispatch(setWillOrganizeDevtalk(false))
+  }
+
+  const handleDevtalkTopic = e => {
+    let topic = e.target.value
+
+    topic && dispatch(setDevtalkTopic(topic))
+  }
+
+  const handleSomethingSpecial = e => {
+    let something_special = e.target.value
+
+    something_special && dispatch(setSomethingSpecial(something_special))
+  }
+
   return (
     <>
       <div>
         <h4>Would you attend Devtalks and maybe also organize your own?</h4>
-        <input type="checkbox" />
+        <input value="yes" onChange={e => handleWillOrganizeDevtalk(e)} type="checkbox" checked={will_organize_devtalk} />
         <label>Yes</label>
 
         <br />
 
-        <input type="checkbox" />
+        <input value="no" onChange={e => handleWillOrganizeDevtalk(e)} type="checkbox" checked={will_organize_devtalk === false} />
         <label>No</label>
 
         <br />
 
         <h4>What would you speak about at Devtalk?</h4>
-        <textarea placeholder="I would..." />
+        <textarea value={devtalk_topic} onChange={e => handleDevtalkTopic(e)} placeholder="I would..." />
 
         <h4>Tell us something special</h4>
-        <textarea placeholder="I..." />
+        <textarea value={something_special} onChange={e => handleSomethingSpecial(e)} placeholder="I..." />
 
 
+        {/* Navbar  */}
         <ul>
           <li><Link to="/covid">Prev</Link></li>
           <li><Link to="/personal-information">personal-information</Link></li>
@@ -34,6 +60,8 @@ const PersonalInformation = () => {
         </ul>
       </div>
 
+
+      {/* Right side */}
       <div>
         <h2>Redberrian Insights</h2>
         <p>
