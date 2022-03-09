9825892da9639714923a53fad080deb8761f6379
Zurab Zaridze <zaridzezurab0@gmail.com>
2022-03-09 22:26:54 +0400

    Added TechnicalSkillset styling

--- src/components/TechnicalSkillset/TechnicalSkillset.js
+++ src/components/TechnicalSkillset/TechnicalSkillset.js
@@ -6,6 +6,9 @@ import { addSkill, delSkill } from "./functions";
 import { validatePage } from "./validatePage";
 import Navigation from "../Navigation";
 
+import styles from './TechnicalSkillset.module.css'
+import RemoveImg from '../../images/skillset/Remove.png'
+
 const TechnicalSkillset = () => {
   // REDUX
   const dispatch = useDispatch();
@@ -27,79 +30,68 @@ const TechnicalSkillset = () => {
   }, [applicant]);
 
   return (
-    <>
-      <div>
+    <div className="Page">
+      <div className="Left">
+        <h2 className="Title">Tell us about your skills</h2>
+
         {/* Skills are added from here */}
-        <h2>Tell us about your skills</h2>
-        <select
-          defaultValue=""
-          onChange={(e) => setTechnology(e.target.value)}
-        >
-          <option value="" disabled>
-            Skills
-          </option>
+        <div className="Form">
+          <select
+            className="Input"
+            defaultValue=""
+            onChange={(e) => setTechnology(e.target.value)}
+          >
+            <option value="" disabled>
+              Skills
+            </option>
 
-          {skills && skills.map((skill) => <option>{skill.title}</option>)}
-        </select>
+            {skills && skills.map((skill) => <option>{skill.title}</option>)}
+          </select>
 
-        <br />
+          <br />
 
-        <input
-          value={experience}
-          onChange={(e) => setExperience(e.target.value)}
-          type="number"
-          placeholder="Experience Duration in Years"
-        />
+          <input
+            className="Input"
+            value={experience}
+            onChange={(e) => setExperience(e.target.value)}
+            type="number"
+            placeholder="Experience Duration in Years"
+          />
 
-        <br />
+          <br />
 
-        <button
-          onClick={() =>
-            addSkill(
-              experience,
-              technology,
-              dispatch,
-              setSkills,
-              applicant,
-              id,
-              setId
-            )
-          }
-        >
-          Add Programming Language
-        </button>
+          <button className={styles.AddButton} onClick={() => addSkill(experience, technology, dispatch, setSkills, applicant, id, setId)}>
+            <p className={styles.Add}>Add Programming Language</p>
+          </button>
+
 
         {/* Added skills are displayed here  */}
-        <ul>
+        <ul className={styles.AddedSkills}>
           {applicant.skills.map((skill) => (
-            <li key={skill.id}>
-              <p>
-                {skill.title} | Years of experience {skill.experience}
-              </p>
-              <button
-                onClick={() =>
-                  delSkill(dispatch, applicant, setSkills, skill.id)
-                }
-              >
-                -
-              </button>
+            <li className="Input" key={skill.id}>
+              <span className={styles.SkillTitle}>{skill.title}</span> <span className={styles.SkillExperience}>Years of experience {skill.experience}</span>
+              <img className={styles.RemoveSkill} onClick={() => delSkill(dispatch, applicant, setSkills, skill.id)} src={RemoveImg} alt="Remove skill" />
             </li>
           ))}
         </ul>
 
+
         <Navigation />
+        </div>
       </div>
 
-      <div>
-        <h2>A bit about out battles</h2>
-        As we said, Redberry has been on the field for quite some time now, and
-        we have touched and embraced a variety of programming languages,
-        technologies, philosophies, and frameworks. We are battle-tested in PHP
-        Laravel Stack with Vue.js, refined in React, and allies with Serverside
-        technologies like Docker and Kubernetes, and now we have set foot in the
-        web3 industry.
+      <div className="Right">
+        <h2 className="Title">A bit about out battles</h2>
+        <p className="Description"> 
+          As we said, Redberry has been on the field for quite some time now, and
+          we have touched and embraced a variety of programming languages,
+          technologies, philosophies, and frameworks. We are battle-tested in PHP
+          Laravel Stack with Vue.js, refined in React, and allies with Serverside
+          technologies like Docker and Kubernetes, and now we have set foot in the
+          web3 industry.
+        </p>
       </div>
-    </>
+    </div>
   );
 };
 
