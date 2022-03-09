0dcb4abb75de5569fe25a0a3c319d8e661c74b72
Zurab Zaridze <zaridzezurab0@gmail.com>
2022-03-09 20:22:26 +0400

    Added PersonalInformation styling

--- src/components/PersonalInformation/PersonalInformation.js
+++ src/components/PersonalInformation/PersonalInformation.js
@@ -10,6 +10,8 @@ import {
 
 import Navigation from "../Navigation";
 
+import styles from "./PersonalInformation.module.css";
+
 const PersonalInformation = () => {
   const dispatch = useDispatch();
   const applicant = useSelector((state) => state.applicant);
@@ -19,49 +21,58 @@ const PersonalInformation = () => {
   }, [applicant]);
 
   return (
-    <>
-      <div>
-        <h2>Hey, Rocketeer, what are your coordinates?</h2>
-        <input
-          value={applicant.first_name}
-          onChange={(e) => handleFirstName(e, dispatch)}
-          type="text"
-          placeholder="First Name"
-        />
-        <br />
-        <input
-          value={applicant.last_name}
-          onChange={(e) => handleLastName(e, dispatch)}
-          type="text"
-          placeholder="Last Name"
-        />
-        <br />
-        <input
-          value={applicant.email}
-          onChange={(e) => handleEmail(e, dispatch)}
-          type="text"
-          placeholder="E Mail"
-        />
-        <br />
-        <input
-          value={applicant.phone}
-          onChange={(e) => handlePhone(e, dispatch)}
-          type="text"
-          placeholder="+995 5__ __ __ __"
-        />
+    <div className="Page">
+      <div className="Left">
+        <h2 className="Title">Hey, Rocketeer, what are your coordinates?</h2>
 
-        <Navigation />
+        <div className={styles.Form}>
+          <input
+            className={`${styles.Input} ${styles.FirstName}`}
+            value={applicant.first_name}
+            onChange={(e) => handleFirstName(e, dispatch)}
+            type="text"
+            placeholder="First Name"
+          />
+          <br />
+          <input
+            className={`${styles.Input} ${styles.LastName}`}
+            value={applicant.last_name}
+            onChange={(e) => handleLastName(e, dispatch)}
+            type="text"
+            placeholder="Last Name"
+          />
+          <br />
+          <input
+            className={`${styles.Input} ${styles.Email}`}
+            value={applicant.email}
+            onChange={(e) => handleEmail(e, dispatch)}
+            type="text"
+            placeholder="E Mail"
+          />
+          <br />
+          <input
+            className={`${styles.Input} ${styles.Phone}`}
+            value={applicant.phone}
+            onChange={(e) => handlePhone(e, dispatch)}
+            type="text"
+            placeholder="+995 5__ __ __ __"
+          />
+
+          <Navigation />
+        </div>
       </div>
 
-      <div>
-        <h2>Redberry Origins</h2>
-        You watch “What? Where? When?” Yeah. Our founders used to play it.
-        That’s where they got a question about a famous American author and
-        screenwriter Ray Bradbury. Albeit, our CEO Gaga Darsalia forgot the
-        exact name and he answered Ray Redberry. And at that moment, a name for
-        a yet to be born company was inspired - Redberry 😇
+      <div className="Right">
+        <h2 className="Title">Redberry Origins</h2>
+        <p className="Description">
+          You watch “What? Where? When?” Yeah. Our founders used to play it.
+          That’s where they got a question about a famous American author and
+          screenwriter Ray Bradbury. Albeit, our CEO Gaga Darsalia forgot the
+          exact name and he answered Ray Redberry. And at that moment, a name
+          for a yet to be born company was inspired - Redberry 😇
+        </p>
       </div>
-    </>
+    </div>
   );
 };
 
