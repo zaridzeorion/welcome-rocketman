c3ae8540f35e5f5725ca9d63092d2b79c140df64
Zurab Zaridze <zaridzezurab0@gmail.com>
2022-03-10 07:58:53 +0400

    Removing applicant data from global Redux state in thank you component

--- src/components/ThankYou/ThankYou.js
+++ src/components/ThankYou/ThankYou.js
@@ -1,14 +1,47 @@
 import React, { useEffect } from "react";
 import styles from "./ThankYou.module.css";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import {
+  setFirstName,
+  setLastName,
+  setEmail,
+  setPhone,
+  setSkills,
+  setWorkPreference,
+  setHadCovid,
+  setHadCovidAt,
+  setVaccinated,
+  setVaccinatedAt,
+  setWillOrganizeDevtalk,
+  setDevtalkTopic,
+  setSomethingSpecial,
+} from "../../store/slices/applicantSlice";
 
 const ThankYou = () => {
+  let dispatch = useDispatch();
   let navigate = useNavigate();
 
   useEffect(() => {
     setTimeout(() => {
       navigate("/");
     }, 3000);
+  }, []);
+
+  useEffect(() => {
+    dispatch(setFirstName(""));
+    dispatch(setLastName(""));
+    dispatch(setEmail(""));
+    dispatch(setPhone(""));
+    dispatch(setSkills(""));
+    dispatch(setWorkPreference(""));
+    dispatch(setHadCovid(""));
+    dispatch(setHadCovidAt(""));
+    dispatch(setVaccinated(""));
+    dispatch(setVaccinatedAt(""));
+    dispatch(setWillOrganizeDevtalk(""));
+    dispatch(setDevtalkTopic(""));
+    dispatch(setSomethingSpecial(""));
   }, []);
 
   return <div className={styles.ThankYou}>Thanks for Joining 😊</div>;
